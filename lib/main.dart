f0b839f611a1199dd51acce848ad7956ba18bde4
engIbrahemQruea <mmany6186@gmail.com>
2025-04-15 22:13:48 +0300

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() {
   runApp(const MyApp());
 }
 
+var myColorScheme2 =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 189, 33, 90));
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,9 +17,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Expanses App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        colorScheme: myColorScheme2,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: myColorScheme2.onPrimaryContainer,
+          foregroundColor: myColorScheme2.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: myColorScheme2.secondaryContainer,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: myColorScheme2.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: myColorScheme2.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: ExpanseHomeView(),
     );
